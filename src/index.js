c2c8fab596d94e39761424168f9b88e8f0abcd28
Vaidant Jadham <vaidantjadham24@gmail.com>
2025-01-29 16:44:44 +0530

    database connect

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello from Index.js");
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+connectDB();
+
+// const app = express();
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (err) => {
+//       console.log("Error :", err);
+//       throw err;
+//     });
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is running on port: ${process.env.PORT}`);
+//     });
+//   } catch (error) {
+//     console.log("Error :", err);
+//   }
+// })();

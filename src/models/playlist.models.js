d141665a4a88ee213f6d6d6b7d92434e24a90d2c
Vaidant Jadham <vaidantjadham24@gmail.com>
2025-02-04 16:12:21 +0530

    add models like comment playlist tweet

---
+++ src/models/playlist.models.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    name: {
+      required: true,
+      type: String,
+    },
+    description: {
+      required: true,
+      type: String,
+    },
+    videos: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: ture }
+);
+
+export const Playlist = mongoose.model("Playlist", playlistSchema);
